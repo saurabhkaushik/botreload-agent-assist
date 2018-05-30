46018c3f57078f61b3380549443d319d3d0cc457
Saurabh Kaushik <saurabh.kaushik.in@gmail.com>
2018-05-30 07:49:04 +0530

    Smart Reply - Impl

--- BR-Helpdesk/agentapp/SmartRepliesSelector.py
+++ BR-Helpdesk/agentapp/SmartRepliesSelector.py
@@ -7,69 +7,85 @@ from nltk.tokenize import RegexpTokenizer
 import numpy as np
 from nltk.corpus import stopwords
 import pandas as pd 
+from sklearn.metrics import pairwise_distances_argmin_min
 from agentapp.model_select import get_model, getTrainingModel, getResponseModel, getCustomerModel
-
+import re
 class SmartRepliesSelector(object):
     
     def createProcessor(self, cust_id):
         self.prepareTrainingData(cust_id)
-        self.ticket_pd['query_cluster'] = self.getKMeanClusters(cust_id, self.X_q, true_k = 10)
-        #self.ticket_pd['response_cluster'] = None 
-        for x in range (0, 10): 
-            query_sub = self.ticket_pd[self.ticket_pd.query_cluster == x]            
-               query_sub['response_cluster'] = self.getKMeanClusters(cust_id, query_sub['response'], true_k = 2) 
-               query_sub.drop(columns=['query', 'tags', 'response', 'query_cluster'])
-               print (query_sub)
-               self.ticket_pd.merge(query_sub, on='id', how='left')
-            #print (self.ticket_pd)
-        #print (self.ticket_pd) 
+        X_q = self.ticket_pd['query']
+        query_clstr_itr = int (len(X_q) / 10) 
+        print ('Query Cluster Size : ', query_clstr_itr)
+        self.ticket_pd['query_cluster'], _, self.ticket_pd['select_tags'] = self.getKMeanClusters(cust_id, X_q, query_clstr_itr)
+        self.ticket_pd['response_cluster'] = -1
+        self.ticket_pd['select_response'] = np.nan
+        if (query_clstr_itr > 1): 
+            for x in range (0, 10): 
+                query_sub = self.ticket_pd[self.ticket_pd.query_cluster == x]            
+                resp_clst_itr = int(len(query_sub) / 5)
+                print ('Response Cluster Size : ',resp_clst_itr)                 
+                if resp_clst_itr > 1:
+                    query_sub['response_cluster'], query_sub['select_response'], ___ = self.getKMeanClusters(cust_id, query_sub['response'], resp_clst_itr) 
+                    for index, items in query_sub.iterrows(): 
+                        self.ticket_pd.loc[(self.ticket_pd['id'] == items['id']), 'response_cluster'] = int (items['response_cluster'])
+                        self.ticket_pd.loc[(self.ticket_pd['id'] == items['id']), 'select_response'] = items['select_response']
+        #print (self.ticket_pd)
+        resp_model = getResponseModel()
+        for index, item in self.ticket_pd.iterrows():             
+            if item['select_response'] == 'true': 
+                resp_model.create(convert(item['select_tags']), convert(item['select_tags']), item['response'], item['select_tags'], done=True, cust_id=cust_id)
+        logging.info('Inserted in Response Data')
         
     def prepareTrainingData(self, cust_id):
         logging.info("prepareTrainingData : " + str(cust_id))
-        self.X_q, self.X_r = [], []
-        #tickets_learn = tickets_learner() 
         ticket_data = self.getTrainingData(cust_id=cust_id)
     
         ticket_struct = []
         for linestms in ticket_data:           
             for linestm in linestms:
-                logging.debug (linestm['tags'] + " =>  " + linestm['resp_category'])
                 ticket_struct.append({'id' : linestm['id'], 'query' : linestm['query'], 'response': linestm['response'], 'tags' : linestm['tags']})
         self.ticket_pd = pd.DataFrame(ticket_struct)
-        self.X_q = self.ticket_pd['query']
-        self.X_r = self.ticket_pd['response']
-
         logging.info ("Total Training Examples : %s" % len(self.ticket_pd))
     
-    def getKMeanClusters(self, cust_id, X_in, true_k = 10):
+    def getKMeanClusters(self, cust_id, X_in, true_k):
         logging.info("getKMeanClusters : " + str(cust_id))
         vectorizer = TfidfVectorizer(stop_words='english')
         try:
             X = vectorizer.fit_transform(X_in)
         except ValueError as err: 
             logging.error(err)
-            return
-            
-        
+            return      
         model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-        model.fit(X)
+        model.fit(X)  
+        prediction = model.predict(X)
+         
+        closest, _ = pairwise_distances_argmin_min(model.cluster_centers_, X)
         
-        '''
-        print("Top terms per cluster:")
         order_centroids = model.cluster_centers_.argsort()[:, ::-1]
         terms = vectorizer.get_feature_names()
+        tags_clurt = []
         for i in range(true_k):
-            print("Cluster %d:" % i),
+            tags_temp = []
             for ind in order_centroids[i, :10]:
-                print(' %s' % terms[ind]),
-            print   
-        print("\n")
-        print ("labels_ : ", model.labels_)
+                tags_temp.append(terms[ind])
+            tags_clurt.append(tags_temp)
         
-        print ("cluster_centers_ : ", model.cluster_centers_)        
-        '''
-        prediction = model.predict(X)
-        return prediction 
+        selected_resp = []
+        selected_tags = []
+        closest = closest.tolist()
+        input_x = X_in.tolist()
+        for i in range(len(prediction)):  
+            qtags = '' 
+            for resp_tags in tags_clurt[prediction[i]]:
+                if resp_tags in input_x[i]:
+                    qtags += resp_tags + ' '
+            selected_tags.append(qtags.strip())
+            if i in closest:  
+                selected_resp.append('true')
+            else :
+                selected_resp.append('false')
+        return prediction, selected_resp, selected_tags 
     
     def getTrainingData(self, cust_id):   
         logging.info ('getTrainingData : ')
@@ -88,3 +104,7 @@ def preprocess(sentence):
     tokens = tokenizer.tokenize(sentence)
     filtered_words = [w for w in tokens if not w in stopwords.words('english')]
     return " ".join(filtered_words)
+
+def convert(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
